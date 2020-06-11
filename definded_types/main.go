ceb5d2a616cad5155bd7fa482d87f25e1b363d96
Dmitry Chekir <chekirdmitry@gmail.com>
2020-06-11 23:24:09 +0300

    methods

---
+++ definded_types/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Gallons float64
+type Liters float64
+
+func (l Liters) toGallons() Gallons {
+	return Gallons(l * 0.264)
+}
+
+func (g Gallons) toLiters() Liters {
+	return Liters(g * 3.785)
+}
+
+func main() {
+	carFuel := Gallons(10.0)
+	busFuel := Liters(240.0)
+
+	fmt.Println(carFuel, busFuel)
+	fmt.Println(carFuel.toLiters(), busFuel.toGallons())
+}
