654542345879dec22ba6fa9392257b6d091c43aa
Dmitry Chekir <chekirdmitry@gmail.com>
2020-06-12 08:12:10 +0300

    methods with links

--- definded_types/main.go
+++ definded_types/main.go
@@ -13,10 +13,17 @@ func (g Gallons) toLiters() Liters {
 	return Liters(g * 3.785)
 }
 
+func (g *Gallons) Double() {
+	*g *= 2
+}
+
 func main() {
 	carFuel := Gallons(10.0)
 	busFuel := Liters(240.0)
 
 	fmt.Println(carFuel, busFuel)
 	fmt.Println(carFuel.toLiters(), busFuel.toGallons())
+
+	carFuel.Double()
+	fmt.Println(carFuel, busFuel)
 }
