930af0b6f6648b29db8d1e4f2d6a3c89f86b596f
Dmitry Chekir <chekirdmitry@gmail.com>
2021-09-25 16:34:24 +0300

    Архив: 116A Трамвай

---
+++ codeforces/archive/A/115_tram/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	n := getInt()
+	scanner := bufio.NewScanner(os.Stdin)
+	var passenger passengers
+	max := 0
+	temp := 0
+
+	for i := 0; i < n; i++ {
+		str := nextString(scanner)
+
+		if str == "" {
+			break
+		}
+
+		passenger = parse(str)
+		temp = temp - passenger.Out + passenger.In
+
+		if temp > max {
+			max = temp
+		}
+	}
+
+	fmt.Println(max)
+
+}
+
+type passengers struct {
+	In  int
+	Out int
+}
+
+func getInt() int {
+	var i int
+	_, err := fmt.Scanf("%d\n", &i)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func nextString(scanner *bufio.Scanner) string {
+	if scanner.Scan() {
+		return scanner.Text()
+	}
+
+	return ""
+}
+
+func parse(input string) passengers {
+
+	arr := strings.Split(input, " ")
+	out, err := strconv.Atoi(arr[0])
+
+	if err != nil {
+		panic(err)
+	}
+
+	in, err := strconv.Atoi(arr[1])
+
+	if err != nil {
+		panic(err)
+	}
+
+	return passengers{In: in, Out: out}
+}
