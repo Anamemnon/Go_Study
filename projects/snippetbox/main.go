cdcbfba9af5bbc37f3ab178508e14e4c6cc87dab
Dmitry Chekir <chekirdmitry@gmail.com>
2021-09-30 22:18:53 +0300

    init snippetbox web app

---
+++ projects/snippetbox/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+const (
+	PORT = 4000
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Printf("Запуск веб-сервера на http://127.0.0.1:%d\n", PORT)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	_, err := w.Write([]byte("Привет"))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Отображение заметки..."))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Форма для создания новой заметки..."))
+
+	if err != nil {
+		panic(err)
+	}
+}
