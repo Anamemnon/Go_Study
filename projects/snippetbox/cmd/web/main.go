26e9ae44db7908b35bd94566041674b5640dd2a5
Dmitry Chekir <chekirdmitry@gmail.com>
2021-10-03 18:35:36 +0300

    Neutered File System was added

--- projects/snippetbox/cmd/web/main.go
+++ projects/snippetbox/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 const (
 	PORT = 4000
@@ -15,10 +16,36 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("projects/snippetbox/ui/static"))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Printf("Запуск веб-сервера на http://127.0.0.1:%d\n", PORT)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
