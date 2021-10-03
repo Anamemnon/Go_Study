eea0b910e435cf052b11e475d6fb4181429c3a3f
Dmitry Chekir <chekirdmitry@gmail.com>
2021-10-03 16:09:18 +0300

    template footer was added

--- projects/snippetbox/cmd/web/handlers.go
+++ projects/snippetbox/cmd/web/handlers.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"projects/snippetbox/ui/html/home.page.tmpl",
 		"projects/snippetbox/ui/html/base.layout.tmpl",
+		"projects/snippetbox/ui/html/footer.partial.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
