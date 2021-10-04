77622b5ea3c0bc8cc72f4a850c389a5ec73ea160
Dmitry Chekir <chekirdmitry@gmail.com>
2021-10-04 15:33:56 +0300

    application struct was added

--- projects/snippetbox/cmd/web/handlers.go
+++ projects/snippetbox/cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -23,19 +22,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
@@ -50,16 +49,16 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 
 		http.Error(w, fmt.Sprintf("%s-метод запрещен", r.Method), http.StatusMethodNotAllowed)
 		return
 	}
-	_, err := w.Write([]byte("Форма для создания новой заметки..."))
+	_, err := w.Write([]byte("Создание новой заметки..."))
 
 	if err != nil {
-		panic(err)
+		app.errorLog.Println(err)
 	}
 }
--- projects/snippetbox/cmd/web/main.go
+++ projects/snippetbox/cmd/web/main.go
@@ -11,6 +11,12 @@ const (
 	ADDRESS = "127.0.0.1:4000"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+
 func main() {
 	addr := flag.String("addr", ADDRESS, "Сетевой адрес HTTP")
 	flag.Parse()
@@ -18,10 +24,15 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
